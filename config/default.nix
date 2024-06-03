c3a4f49fb67ffe7643d2d14a5466e28c0298e7e9
AdrisGithub <adrianjohn6543@gmail.com>
2024-06-03 23:31:47 +0200

    init commit

---
+++ config/default.nix
@@ -0,0 +1,85 @@
+{
+  # Import all your configuration modules here
+  imports = [ ./bufferline.nix ];
+
+  colorschemes.catppuccin.enable = true;
+  plugins.lsp = {
+    enable = true;
+    servers = {
+      nil-ls.enable = true;
+      #	rust-analyzer.enable = true;
+      rust-analyzer = {
+        enable = true;
+        installCargo = true;
+        installRustc = true;
+        settings = {
+          check.command = "check";
+        };
+      };
+
+    };
+  };
+  plugins.cmp = {
+    enable = true;
+    autoEnableSources = true;
+    settings = {
+
+      mapping = {
+        "<CR>" = "cmp.mapping.confirm({ select = true })";
+        "<Tab>" = ''
+          cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif require("luasnip").expand_or_locally_jumpable() then
+              require("luasnip").expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" })
+        '';
+        "<S-Tab>" = ''
+          cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif require("luasnip").jumpable(-1) then
+              require("luasnip").jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" })
+        '';
+        "<Up>" = "cmp.mapping.select_prev_item()";
+        "<Down>" = "cmp.mapping.select_next_item()";
+      };
+      snippet.expand = ''
+        function(args)
+          require('luasnip').lsp_expand(args.body)
+        end
+      '';
+      sources = [
+        { name = "nvim_lsp"; }
+        { name = "luasnip"; }
+        { name = "path"; }
+        { name = "buffer"; }
+      ];
+    };
+  };
+  plugins.lualine.enable = true;
+  plugins.cmp-path.enable = true;
+  plugins.cmp-buffer.enable = true;
+  plugins.cmp_luasnip.enable = true;
+  plugins.cmp-nvim-lua.enable = true;
+  plugins.luasnip = {
+    enable = true;
+    fromVscode = [{ }];
+  };
+  extraConfigLuaPre = ''
+    local has_words_before = function()
+      unpack = unpack or table.unpack
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    end
+  '';
+}
